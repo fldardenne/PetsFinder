7c61f05f1ef7fc4c5fce6546ca94f260b276f6ba
fdardenne <dardenneflorent@outlook.com>
2019-05-18 23:43:53 +0200

    Edit a post, delete an account

--- routes/post.js
+++ routes/post.js
@@ -3,6 +3,7 @@ let router = express.Router();
 let Post = require('../models/post')
 let User = require('../models/user')
 var path = require('path');
+var moment = require('moment');
 
 //upload image
 const multer = require("multer");
@@ -84,7 +85,61 @@ router.get('/delete/:postID', (req, res) => {
             }
         })
     })
-
 })
+
+router.get('/edit/:postID', (req, res) => {
+    var id = req.params.postID;
+    Post.findById(id, (err, post_doc) => {
+        User.findOne({mail: req.session.mail}, (err, acc_doc) => {
+            if (post_doc.author == acc_doc.id) {
+                res.render('post/edit', {
+                    post: post_doc,
+                    date: moment(post_doc.date).format('YYYY-MM-DD'),
+                    session: req.session
+                });
+            }else{
+                res.redirect('/post');
+            }
+        })
+    })
+})
+
+router.post('/edit/:postID', (req,res) => {
+    upload(req,res,(err) => {
+        if(err){
+            res.json(err);
+        }
+        User.findOne({mail: req.session.mail}, (err, doc_acc) => {
+            Post.findById(req.params.postID, (err, post_doc) => {
+                
+                post_doc.author = doc_acc;
+                post_doc.petname = req.body.petname;
+                post_doc.location = req.body.location;
+                post_doc.date = req.body.date_lost;
+                post_doc.description = req.body.description;
+                if(req.body.found == "Found"){
+                    post_doc.found = true;
+                }else{
+                    post_doc.found = false;
+                }
+                
+                post_doc.tags = req.body.tags;
+                if(req.file){
+                    post_doc.thumbnail = "/uploads/" + req.file.filename;
+                }
+
+                post_doc.save(function(err) {
+                    console.log("saved");
+                    if (err) res.json(err);
+
+                    res.redirect('/post');
+                });
+            })
+            
+            
+        });
+
+    });
+});
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let router = express.Router();
 const bcrypt = require('bcrypt');
+let User = require('../models/user');
+let Post = require('../models/post');
 
 
 router.get('/delete', (req, res) => {
@@ -9,4 +11,18 @@ router.get('/delete', (req, res) => {
     });
 })
 
+router.post('/delete', (req,res) => {
+    User.find({mail: req.session.mail}, (err, acc_doc) => {
+        Post.find({author:acc_doc}).remove().then(function(value) {
+            User.find({mail: req.session.mail}).remove().then(function(value){
+                req.session.mail = '';
+                req.session.save((err) => {
+                    res.redirect('/');
+                })
+            })
+        });
+    })
+    
+    
+})
 module.exports = router;

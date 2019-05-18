4ceca95b9941f6f5a30f827353d2d908c3bdbe73
fdardenne <dardenneflorent@outlook.com>
2019-05-19 01:18:19 +0200

    Phone edit ok

--- routes/phone.js
+++ routes/phone.js
@@ -1,14 +1,27 @@
 let express = require('express');
 let router = express.Router();
+let User = require('../models/user');
 
 router.get('/edit', (req, res) => {
-    res.render('phone/edit', {
-        session: req.session
+    User.findOne({mail: req.session.mail}, (err, doc_acc) => {
+        res.render('phone/edit', {
+            session: req.session,
+            phone: doc_acc.phone
+        })
     })
 })
 
 router.post('/edit', (req, res) => {
-    res.send('Logged');
+    User.findOne({mail: req.session.mail}, (err, doc_acc) => {
+        doc_acc.phone = req.body.phone;
+        doc_acc.save(function(value){
+            res.render('phone/edit', {
+                session: req.session,
+                phone: doc_acc.phone
+            });
+        });
+        
+    })
 })
 
 module.exports = router;

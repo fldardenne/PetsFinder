1162dd4dbbcc0c3e8085cfc660e288c2e1ea5404
fdardenne <dardenneflorent@outlook.com>
2019-05-19 19:52:33 +0200

    Refactor the map in a separate js file, remove Google autocomplete

--- public/js/autocomplete.js
+++
@@ -1,6 +0,0 @@
-var options = {
-    types: ['(cities)'],
-    componentRestrictions: {country: 'ca'}
- };
- var input = document.getElementById('location');
- autocomplete = new google.maps.places.Autocomplete(input, options);
